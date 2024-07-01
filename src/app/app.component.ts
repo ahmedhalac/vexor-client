0cacd4132b9058d2bdda1e5a0ba6f74a7966ce34
ahmedhalac <ahmedhalac@outlook.com>
2024-07-02 00:03:27 +0200

    Added fonts, styling login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,14 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'vexor-client';
 
   constructor(private translate: TranslateService) {
+    translate.addLangs(['en', 'klingon']);
+    translate.addLangs(['bos', 'klingon']);
     translate.setDefaultLang('en');
     translate.use('en');
   }
